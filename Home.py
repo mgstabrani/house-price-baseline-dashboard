b88591171bf9146b455ce27510213d44592d7dda
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-11-29 09:30:22 +0700

    feature: multiple pages

---
+++ Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="House Pricing Baseline",
+    page_icon="👋",
+)
+
+st.write("# Welcome to House Pricing Baseline! 👋")
+
+st.markdown(
+    """
+    ### Contributors
+    - Yusuf Alwansyah Hilmy (13519005)
+    - Ferdy Irawan Firdaus (13519030)
+    - Muhammad Fahkry Malta (13519032)
+    - Ridho Daffasyah (13519038)
+    - Thomas Ferdinand Martin (13519099)
+    - Mgs. Tabrani (13519122)
+    - Muhammad Rifky Muthahhari (13519123)
+    """
+)
