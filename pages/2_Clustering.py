ed2024d544ea83277bcaeaab2beb5af6037b25d6
Mgs. Tabrani <mgstabrani19@gmail.com>
2022-11-29 09:57:42 +0700

    feature: init input params for clustering

--- pages/2_Clustering.py
+++ pages/2_Clustering.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import importlib
+module = importlib.import_module('pages.1_Regresi')
+input_user = module.input_user
 
 st.set_page_config(layout='wide')
 st.title('Clustering')
+
+df = input_user()
+
+# Print specified input parameters
+st.header('Specified Input parameters')
+st.write(df)
+st.write('---')
